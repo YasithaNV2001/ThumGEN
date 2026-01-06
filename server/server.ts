4e6b457abda9fdb7df7008985009cdf3e8c0325c
Yasitha NV <mryasithanv@gmail.com>
2026-01-06 20:44:28 +0530

    url_update

--- server/server.ts
+++ server/server.ts
@@ -27,7 +27,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 'http://localhost:3000' , 'https://thum-gen-client.vercel.app'],
     credentials: true,
 }));
 
